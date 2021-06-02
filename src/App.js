aacc791d78844c74dd199f20c87c61d640e05f12
grace <grace.c.youngblood@gmail.com>
2021-06-02 16:37:26 -0400

    Add a `mobx` memory model, re-rendering is broken.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import Grid from "react-data-grid"
+import { makeAutoObservable, autorun } from "mobx"
+import { Observer, observer } from "mobx-react"
+
+class Memory {
+  members = []
+
+  constructor() {
+    makeAutoObservable(this)
+    autorun(() => console.log(this.members.length, "members in memory"))
+  }
+}
+
+var memory = new Memory()
+
+// setTimeout(() => {
+  memory.members.push({
+    name: "Alma S. Adams",
+    image: "https://www.congress.gov/img/member/a000370_200.jpg",
+  })
+// }, 1000)
 
 var columns = [
   { key: "name", name: "Name" },
-  { key: "image", name: "Image" },
+  {
+    key: "image",
+    name: "Image",
+    formatter: ({ row }) => <img style={{ height: "120px" }} src={row.image} alt={row.name} />,
+  },
 ]
 
 var rows = [
-  {
-    name: "Alma S. Adams",
-    image: <img style={{ height: "120px" }} src="https://www.congress.gov/img/member/a000370_200.jpg" alt="Alma S. Adams" />,
-  },
 ]
 
 function App() {
   return (
-    <div>
-      <Grid columns={columns} rows={rows} rowHeight={() => 120 }/>
-    </div>
+    <Observer>{() => (
+      <div>
+        <Grid columns={columns} rows={memory.members} rowHeight={() => 120 }/>
+      </div>
+    )}</Observer>
   );
 }
 
-export default App;
+window.memory = memory
+
+export default observer(App)
