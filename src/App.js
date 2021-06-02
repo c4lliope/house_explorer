ad23ae6c097f69eb3b49369f3d6891c8c307baa7
grace <grace.c.youngblood@gmail.com>
2021-06-02 16:23:21 -0400

    Add a grid on page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Grid from "react-data-grid"
+
+var columns = [
+  { key: "name", name: "Name" },
+  { key: "image", name: "Image" },
+]
+
+var rows = [
+  {
+    name: "Alma S. Adams",
+    image: <img style={{ height: "120px" }} src="https://www.congress.gov/img/member/a000370_200.jpg" alt="Alma S. Adams" />,
+  },
+]
+
 function App() {
   return (
     <div>
-      <img src="https://www.congress.gov/img/member/a000370_200.jpg" alt="Alma S. Adams" />
-      <p>Alma S. Adams</p>
+      <Grid columns={columns} rows={rows} rowHeight={() => 120 }/>
     </div>
   );
 }
