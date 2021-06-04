a2169f4f5f65e96da66dc9ed1c0c4980f0d131d9
grace <grace.c.youngblood@gmail.com>
2021-06-04 19:32:19 -0400

    Middle-align span.

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ var App = () => (
       </div>
 
       <h1>U.S. House of Representatives</h1>
-      <span>Roll Call Record</span>
+      <span style={{ textAlign: "center" }}>Roll Call Record</span>
     </Heading>
     <Grid columns={columns} rows={memory.votes} />
   </Page>
